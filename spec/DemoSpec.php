a3d0d2b86c42c646737e676974d5331e6a2a87d1
scaphandroid <arl-informatique@gmx.ch>
2016-10-11 21:31:29 +0200

    tests avec kahlan

---
+++ spec/DemoSpec.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexis
+ * Date: 11/10/2016
+ * Time: 20:34
+ */
+
+describe('Demo', function(){
+
+    it('should addition 1+1', function(){
+        expect(1+1)->toBe(2);
+    });
+});
+
+describe('tests d\'une réservation', function(){
+    it('on crée une réservation et on lui ajoute un billet', function(){
+        expect(function(){
+            $resa = new \AR\LouvreBundle\Entity\Reservation();
+            $resa->setNbBillets(1);
+            $nbBillet = $resa->getNbBillets();
+        })->toBe(1);
+    });
+});
