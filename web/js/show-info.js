64f14ced18664bbc9c67bec672885937dcf5ecc9
scaphandroid <arl-informatique@gmx.ch>
2016-10-22 18:35:30 +0200

    add a info section and javascript to toggle

---
+++ web/js/show-info.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alexis on 22/10/2016.
+ */
+$(document).ready(function(){
+
+    $('#info-toggle').click(function(){
+
+        $('#hidden-info').toggle();
+        $('#content-main').toggle();
+    });
+});
