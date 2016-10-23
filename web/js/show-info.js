290cfac9613946753d471454988480b97665a079
scaphandroid <arl-informatique@gmx.ch>
2016-10-23 19:31:22 +0200

    complete info tarifs and toggle

--- web/js/show-info.js
+++ web/js/show-info.js
@@ -3,11 +3,45 @@
  */
 $(document).ready(function(){
 
+    var tarifActif = false;
+    var infoActif = false;
+
+    $('.show-resa').click(function (e){
+
+        e.preventDefault();
+
+        showAndHide('#content-main', '#hidden-tarifs', '#hidden-info');
+    });
+
+
     $('.info-toggle').click(function(e){
 
         e.preventDefault();
 
-        $('#hidden-info').toggle();
-        $('#content-main').toggle();
+        infoActif = !infoActif;
+
+        showAndHide('#hidden-info', '#hidden-tarifs', '#content-main');
     });
+
+    $('.tarifs-toggle').click(function(e){
+
+        e.preventDefault();
+
+        tarifActif = !tarifActif;
+
+        showAndHide('#hidden-tarifs', '#hidden-info', '#content-main');
+    });
+
+    function showAndHide(el1, el2, el3){
+
+        if($(el1).css('display') == 'none'){
+            $(el1).show();
+            $(el2).hide();
+            $(el3).hide();
+        }else{
+            $('#hidden-info').hide();
+            $('#hidden-tarifs').hide();
+            $('#content-main').show();
+        }
+    }
 });
